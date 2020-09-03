b2ccc7ac746cb684ff4af714cfa9a228d6fc365c
Michael Stallings <stall84@gmail.com>
2020-09-02 20:02:17 -0400

    added test package and files. having issues getting test to run

--- src/GradeBook/Book.cs
+++ src/GradeBook/Book.cs
@@ -22,6 +22,37 @@ namespace GradeBook
             grades.Add(grade);
         }
 
+        public void ShowStatistics()
+        {
+
+            // We are going to compare all of the grades in the grades List by looping through. To determine the highest and lowest grades we're
+            // first going to initialize their variable-holders to the lowest and highest possible double values possible. That way when the loop starts,
+            // the first value in the loop will have to be higher or lower respectively and thus stored in the variable, till the next value is compared.
+            // this is a more sure and complete method than setting them to just zero.
+            double listSum = 0;
+            int elems = grades.Count;
+            double highGrade = double.MinValue;
+            double lowGrade = double.MaxValue;
+
+            foreach (double num in grades)
+            {
+                // Math.Max/Min will take two params. the current number/value in List being iterated over, and the current value for their high/low-grade variable.
+                // Will retun into that variable the higher or lower of the two.
+                highGrade = Math.Max(num, highGrade);
+                lowGrade = Math.Min(num, lowGrade);
+                listSum += num;
+            }
+            var listResult = listSum / elems;
+            Console.WriteLine($"The average grade is: {listResult:N1}");
+            Console.WriteLine($"The lowest grade is: {lowGrade}");
+            Console.WriteLine($"The highest grade is: {highGrade}");
+
+        }
+
+        // Using the static keyword on these fields would make them inaccessible from any object instantiations (new's) .. and would make them available
+        // only on the Class type (Book).. This kind of defeats the purpose of object-oriented programming and should only be used in rare circumstances.
+        // Otherwise your fields should be defined as 'instance' occuring like below where a new object has to be instantiated for them to be used. 
+        // thus keeping them 'encapsulated' and sealed off from the outside programming world (safer).
         List<double> grades;
         string name;
 
--- src/GradeBook/Program.cs
+++ src/GradeBook/Program.cs
@@ -13,18 +13,12 @@ namespace GradeBook
             var book = new Book("Michael's Gradebook");
             book.AddGrade(90.2);
             book.AddGrade(95.3);
+            book.AddGrade(98.2);
+            book.ShowStatistics();
 
-            // Lists
-            List<double> grades = new List<double>() { 21.45, 36.33, 58.72, 63.71, 72.97, 88.39 };
-            double listSum = 0;
-            int elems = grades.Count;
 
-            foreach (double num in grades)
-            {
-                listSum += num;
-            }
-            var listResult = listSum / elems;
-            Console.WriteLine($"The average grade is: {listResult:N2}");
+
+
 
 
         }
